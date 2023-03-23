6098f79566f2bc1af258613e1a9e2358152cb082
RobertJunas <rob.junas@gmail.com>
2023-03-23 10:42:10 +0100

    vytvorenie get endopintov

--- backend/Controllers/EmployeeController.cs
+++ backend/Controllers/EmployeeController.cs
@@ -25,11 +25,46 @@ namespace backend.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Employee>>> GetEmployees()
         {
+
           if (_context.Employees == null)
           {
               return NotFound();
           }
             return await _context.Employees.ToListAsync();
+        }
+
+        [HttpGet("actual/")]
+        public async Task<ActionResult<IEnumerable<Employee>>> GetEmployed()
+        {
+          if (_context.Employees == null)
+          {
+              return NotFound();
+          }
+
+          var employees = from e in _context.Employees
+                            join ps in _context.PositionsEmployees
+                                on e.id equals ps.EmployeeId into empPos
+                            from pe in empPos.DefaultIfEmpty()
+                            where pe.End == null 
+                            select e;
+        return await employees.ToListAsync();
+        }
+
+        [HttpGet("archived/")]
+        public async Task<ActionResult<IEnumerable<Employee>>> GetArchivedEmployees()
+        {
+          if (_context.Employees == null)
+          {
+              return NotFound();
+          }
+
+          var employees = _context.Employees.Except(from e in _context.Employees
+                            join ps in _context.PositionsEmployees
+                                on e.id equals ps.EmployeeId into empPos
+                            from pe in empPos.DefaultIfEmpty()
+                            where pe.End == null 
+                            select e);
+        return await employees.ToListAsync();
         }
 
         // GET: api/Employee/5
@@ -53,15 +88,72 @@ namespace backend.Controllers
         // PUT: api/Employee/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutEmployees(long id, Employee employees)
+        public async Task<IActionResult> PutEmployees(long id, EmployeeCreation e)
         {
-            if (id != employees.id)
+            var employee = await _context.Employees.FindAsync(id);
+            if(employee == null){
+                return NotFound();
+            }
+            List<PositionsEmployee> p = employee.Positions.ToList();
+            var index = p.FindIndex(i=> i.End == null);
+
+            if(DateOnly.FromDateTime(DateTime.Now) <= e.DateOfBirth){
+                return BadRequest($"Date of birth should be smaller than {DateOnly.FromDateTime(DateTime.Now)}");
+            }
+            if(e.StartingDate != employee.StartingDate && e.PositionId != p[index].PositionId && DateOnly.FromDateTime(DateTime.Now) > e.StartingDate){
+                return BadRequest($"Starting date should be greater or equal to {DateOnly.FromDateTime(DateTime.Now)}");
+            }
+                
+            employee.Name = e.Name;
+            employee.LastName = e.LastName;
+            employee.Address = e.Address;
+            employee.DateOfBirth = e.DateOfBirth;
+            employee.StartingDate = e.StartingDate;
+            employee.Salary = e.Salary;
+
+
+            p[index].End = e.StartingDate;
+            p.Add(new PositionsEmployee(){
+                Employee = employee,
+                PositionId = e.PositionId,
+                Position = await _context.Positions.FindAsync(e.PositionId),
+                Start = e.StartingDate
+            });
+            employee.Positions = p;
+            try
             {
-                return BadRequest();
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EmployeesExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
             }
 
-            _context.Entry(employees).State = EntityState.Modified;
+            return NoContent();
+        }
 
+        [HttpPut("{id}/archive")]
+        public async Task<IActionResult> archiveEmployees(long id)
+        {
+            var employee = await _context.Employees.FindAsync(id);
+            if(employee == null){
+                return NotFound();
+            }
+            List<PositionsEmployee> p = employee.Positions.ToList();
+            var index = p.FindIndex(i=> i.End == null);
+            if(index == -1){
+                return NotFound();
+            }
+
+            p[index].End = DateOnly.FromDateTime(DateTime.Now);
+            employee.Positions = p;
             try
             {
                 await _context.SaveChangesAsync();
